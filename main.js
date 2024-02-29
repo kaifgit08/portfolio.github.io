6ee04888de961dc6b91ad2f069ff2ae82445e017
Mohammed Kaif <kaifgit08@gmail.com>
2024-02-29 20:18:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+  strings: ["hi", "FullStack Developer", "Web Designer"],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+var typing = new Typed(".text1", {
+  strings: ["Hola,", " Bonjour,", "Hallo,", "Nǐ hǎo,", "S̄wạs̄dī,", " Ciao,"],
+  typedSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
